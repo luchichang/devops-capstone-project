bfa3e2e157b77095d6b0e1c2d407ba11dd8ff2fd
luchichang <dmerism006@gmail.com>
2024-12-19 07:37:04 -0500

    account update feature completed

--- service/routes.py
+++ service/routes.py
@@ -85,7 +85,21 @@ def get_accounts(account_id):
 # UPDATE AN EXISTING ACCOUNT
 ######################################################################
 
-# ... place you code here to UPDATE an account ...
+@app.route("/accounts/<int:account_id>", methods=["PUT"])
+def update_accounts(account_id):
+    """
+    update an Account
+    This endpoint will account based on the posted data
+    """
+    app.logger.info("Request to update an Account with id: %s", account_id)
+
+    account = Account.find(account_id)
+    if not account:
+        abort(status.HTTP_404_NOT_FOUND, f"Account with id {account_id} cannot be found")
+    # updating the account
+    account.deserialize(request.get_json())
+    account.update()
+    return account.serialize(), status.HTTP_200_OK
 
 
 ######################################################################
